62ab6a90cc235786ef5c3413c3b36511609d707b
Mathieu Delisle <mdelisle@webonaute.ca>
2018-06-06 18:58:00 -0400

    Fix service infinit loop.

--- Topic/TopicManager.php
+++ Topic/TopicManager.php
@@ -25,16 +25,6 @@ class TopicManager implements WsServerInterface, WampServerInterface
      */
     protected $topicLookup = [];
 
-    /**
-     * TopicManager constructor.
-     *
-     * @param WampApplication $wampApplication
-     */
-    public function __construct(WampApplication $wampApplication)
-    {
-        $this->app = $wampApplication;
-    }
-
     public function setWampApplication(WampServerInterface $app)
     {
         $this->app = $app;
