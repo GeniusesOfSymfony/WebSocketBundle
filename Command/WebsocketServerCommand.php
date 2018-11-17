7a420e33c8639d2de7911efcfdad27baeea03ecd
Michael Babker <michael.babker@gmail.com>
2018-11-17 14:44:08 -0600

    Remove unused Logger from the WebsocketServerCommand

--- Command/WebsocketServerCommand.php
+++ Command/WebsocketServerCommand.php
@@ -3,8 +3,6 @@
 namespace Gos\Bundle\WebSocketBundle\Command;
 
 use Gos\Bundle\WebSocketBundle\Server\EntryPoint;
-use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -19,11 +17,6 @@ class WebsocketServerCommand extends Command
     protected $entryPoint;
 
     /**
-     * @var LoggerInterface
-     */
-    protected $logger;
-
-    /**
      * @var string
      */
     protected $host;
@@ -34,21 +27,15 @@ class WebsocketServerCommand extends Command
     protected $port;
 
     /**
-     * @param EntryPoint      $entryPoint
-     * @param string          $host
-     * @param int             $port
-     * @param LoggerInterface $logger
+     * @param EntryPoint $entryPoint
+     * @param string     $host
+     * @param int        $port
      */
-    public function __construct(
-        EntryPoint $entryPoint,
-        $host,
-        $port,
-        LoggerInterface $logger = null
-    ) {
+    public function __construct(EntryPoint $entryPoint, $host, $port)
+    {
         $this->entryPoint = $entryPoint;
         $this->port = (int) $port;
         $this->host = $host;
-        $this->logger = null === $logger ? new NullLogger() : $logger;
 
         parent::__construct();
     }
