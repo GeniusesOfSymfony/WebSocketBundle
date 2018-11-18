a2e77287204133a402bc5064f8d6ae025bee632b
Michael Babker <michael.babker@gmail.com>
2018-11-18 15:17:29 -0600

    Test WebsocketServerCommand, use scalar typehinting on the constructor, make command lazy, declare Console component dependency

--- Command/WebsocketServerCommand.php
+++ Command/WebsocketServerCommand.php
@@ -11,6 +11,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class WebsocketServerCommand extends Command
 {
+    protected static $defaultName = 'gos:websocket:server';
+
     /**
      * @var EntryPoint
      */
@@ -26,35 +28,25 @@ class WebsocketServerCommand extends Command
      */
     protected $port;
 
-    /**
-     * @param EntryPoint $entryPoint
-     * @param string     $host
-     * @param int        $port
-     */
-    public function __construct(EntryPoint $entryPoint, $host, $port)
+    public function __construct(EntryPoint $entryPoint, string$host, int $port)
     {
-        $this->entryPoint = $entryPoint;
-        $this->port = (int) $port;
-        $this->host = $host;
-
         parent::__construct();
+
+        $this->entryPoint = $entryPoint;
+        $this->port = $port;
+        $this->host = $host;
     }
 
     protected function configure()
     {
         $this
-            ->setName('gos:websocket:server')
-            ->setDescription('Starts the web socket servers')
-            ->addArgument('name', InputArgument::OPTIONAL, 'Server name')
-            ->addOption('profile', 'm', InputOption::VALUE_NONE, 'Profiling server')
-            ->addOption('host', 'a', InputOption::VALUE_OPTIONAL, 'Host')
-            ->addOption('port', 'p', InputOption::VALUE_OPTIONAL, 'port');
+            ->setDescription('Starts the websocket server')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Name of the server to start, launches the first registered server if not specified')
+            ->addOption('profile', 'm', InputOption::VALUE_NONE, 'Enable profiling of the server')
+            ->addOption('host', 'a', InputOption::VALUE_OPTIONAL, 'The hostname of the websocket server')
+            ->addOption('port', 'p', InputOption::VALUE_OPTIONAL, 'The port of the websocket server');
     }
 
-    /**
-     * @param InputInterface  $input
-     * @param OutputInterface $output
-     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->entryPoint->launch(
---
+++ Tests/Command/WebsocketServerCommandTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Gos\Bundle\WebSocketBundle\Tests\Command;
+
+use Gos\Bundle\PubSubRouterBundle\Router\RouterRegistry;
+use Gos\Bundle\WebSocketBundle\Command\WebsocketServerCommand;
+use Gos\Bundle\WebSocketBundle\Server\EntryPoint;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class WebsocketServerCommandTest extends TestCase
+{
+    public function testCommandLaunchesWebSocketServer()
+    {
+        $entryPoint = $this->createMock(EntryPoint::class);
+        $entryPoint->expects($this->once())
+            ->method('launch')
+            ->with(null, 'localhost', 1337, false);
+
+        $command = new WebsocketServerCommand($entryPoint, 'localhost', 1337);
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+    }
+
+    public function testCommandLaunchesWebSocketServerWithConsoleArgumentsAndOptions()
+    {
+        $entryPoint = $this->createMock(EntryPoint::class);
+        $entryPoint->expects($this->once())
+            ->method('launch')
+            ->with('websocket', 'web.socket', 8443, true);
+
+        $command = new WebsocketServerCommand($entryPoint, 'localhost', 1337);
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+            [
+                'name' => 'websocket',
+                '--host' => 'web.socket',
+                '--port' => 8443,
+                '--profile' => true,
+            ]
+        );
+    }
+}
