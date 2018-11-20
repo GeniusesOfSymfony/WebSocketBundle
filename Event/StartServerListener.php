1952088f99291bc8e847d8c3a0cc8ac12901df10
Michael Babker <michael.babker@gmail.com>
2018-11-19 21:41:05 -0600

    Inline the Closure

--- Event/StartServerListener.php
+++ Event/StartServerListener.php
@@ -6,9 +6,7 @@ use Gos\Bundle\WebSocketBundle\Pusher\ServerPushHandlerRegistry;
 use Gos\Bundle\WebSocketBundle\Server\App\Registry\PeriodicRegistry;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
-use React\EventLoop\LoopInterface;
 use React\EventLoop\TimerInterface;
-use React\Socket\Server;
 
 class StartServerListener
 {
@@ -43,33 +41,6 @@ class StartServerListener
     }
 
     /**
-     * @param Server        $server
-     * @param LoopInterface $loop
-     */
-    protected function closure(Server $server, LoopInterface $loop)
-    {
-        $this->logger->notice('Stopping server ...');
-
-        foreach ($this->serverPushHandlerRegistry->getPushers() as $handler) {
-            $handler->close();
-            $this->logger->info(sprintf('Stop %s push handler', $handler->getName()));
-        }
-
-        $server->emit('end');
-        $server->close();
-
-        foreach ($this->periodicRegistry->getPeriodics() as $periodic) {
-            if ($periodic instanceof TimerInterface) {
-                $loop->cancelTimer($periodic);
-            }
-        }
-
-        $loop->stop();
-
-        $this->logger->notice('Server stopped!');
-    }
-
-    /**
      * @param ServerEvent $event
      */
     public function bindPnctlEvent(ServerEvent $event)
@@ -79,7 +50,25 @@ class StartServerListener
 
         if (defined('SIGINT')) {
             $loop->addSignal(SIGINT, function () use ($server, $loop) {
-                $this->closure($server, $loop);
+                $this->logger->notice('Stopping server ...');
+
+                foreach ($this->serverPushHandlerRegistry->getPushers() as $handler) {
+                    $handler->close();
+                    $this->logger->info(sprintf('Stop %s push handler', $handler->getName()));
+                }
+
+                $server->emit('end');
+                $server->close();
+
+                foreach ($this->periodicRegistry->getPeriodics() as $periodic) {
+                    if ($periodic instanceof TimerInterface) {
+                        $loop->cancelTimer($periodic);
+                    }
+                }
+
+                $loop->stop();
+
+                $this->logger->notice('Server stopped!');
             });
         }
     }
