ae9ccee24b99e444f2ac8320bb70f5ca5d394736
Michael Babker <michael.babker@gmail.com>
2019-01-01 18:07:12 -0600

    Deprecate support for the ZMQ pusher

--- DependencyInjection/GosWebSocketExtension.php
+++ DependencyInjection/GosWebSocketExtension.php
@@ -5,6 +5,7 @@ namespace Gos\Bundle\WebSocketBundle\DependencyInjection;
 use Monolog\Logger;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Reference;
@@ -175,10 +176,31 @@ class GosWebSocketExtension extends Extension implements PrependExtensionInterfa
                 ->replaceArgument(0, new Reference('gos_pubsub_router.websocket'));
         }
 
-        // WAMP Pusher Configuration
-        if (isset($configs['pushers']) && isset($configs['pushers']['wamp'])) {
-            if (!is_bool($configs['pushers']['wamp']['ssl'])) {
-                throw new \InvalidArgumentException(sprintf('The ssl node under wamp pusher configuration must be a boolean value'));
+        // Pusher Configuration
+        if (isset($configs['pushers'])) {
+            // Validate WAMP configuration
+            if (isset($configs['pushers']['wamp'])) {
+                if (!is_bool($configs['pushers']['wamp']['ssl'])) {
+                    throw new \InvalidArgumentException(
+                        sprintf('The ssl node under wamp pusher configuration must be a boolean value')
+                    );
+                }
+            }
+
+            // Deprecate ZMQ pusher
+            if (isset($configs['pushers']['zmq'])) {
+                @trigger_error(
+                    'Support for the ZMQ pusher is deprecated and will be removed in 2.0.',
+                    E_USER_DEPRECATED
+                );
+
+                if (method_exists(Definition::class, 'setDeprecated')) {
+                    $pusherDef = $container->getDefinition('gos_web_socket.zmq.pusher');
+                    $pusherDef->setDeprecated(true);
+
+                    $pushHandlerDef = $container->getDefinition('gos_web_socket.zmq.server_push_handler');
+                    $pushHandlerDef->setDeprecated(true);
+                }
             }
         }
     }
