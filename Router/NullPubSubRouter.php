0191307531eafe518353b1492d30234d3caabf35
Michael Babker <michael.babker@gmail.com>
2019-01-01 19:40:12 -0600

    Update the null router

--- Router/NullPubSubRouter.php
+++ Router/NullPubSubRouter.php
@@ -3,16 +3,11 @@
 namespace Gos\Bundle\WebSocketBundle\Router;
 
 use Gos\Bundle\PubSubRouterBundle\Router\RouteCollection;
-use Gos\Bundle\PubSubRouterBundle\Router\RouteInterface;
-use Gos\Bundle\PubSubRouterBundle\Router\RouterContext;
 use Gos\Bundle\PubSubRouterBundle\Router\RouterInterface;
 
 class NullPubSubRouter implements RouterInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function generate($routeName, Array $parameters = [], $tokenSeparator = null)
+    public function generate(string $routeName, array $parameters = []): string
     {
         throw new \Exception('Websocket router is not configured, see doc');
     }
@@ -20,55 +15,17 @@ class NullPubSubRouter implements RouterInterface
     /**
      * {@inheritdoc}
      */
-    public function generateFromTokens(RouteInterface $route, Array $tokens, Array $parameters = [], $tokenSeparator)
+    public function match(string $channel): array
     {
         throw new \Exception('Websocket router is not configured, see doc');
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function match($channel, $tokenSeparator = null)
-    {
-        throw new \Exception('Websocket router is not configured, see doc');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setCollection(RouteCollection $collection)
-    {
-        throw new \Exception('Websocket router is not configured, see doc');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setContext(RouterContext $context)
-    {
-        throw new \Exception('Websocket router is not configured, see doc');
-    }
-
-    /**
-     * @return RouterContext
-     */
-    public function getContext()
-    {
-        // TODO: Implement getContext() method.
-    }
-
-    /**
-     * @return RouteCollection
-     */
-    public function getCollection()
+    public function getCollection(): RouteCollection
     {
         // TODO: Implement getCollection() method.
     }
 
-    /**
-     * @return string
-     */
-    public function getName()
+    public function getName(): string
     {
         // TODO: Implement getName() method.
     }
