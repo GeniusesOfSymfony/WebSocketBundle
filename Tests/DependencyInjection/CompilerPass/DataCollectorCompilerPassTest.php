8f2492b11ff7969e199826aec315f34f67a189b8
Michael Babker <michael.babker@gmail.com>
2019-09-03 15:05:52 -0500

    Tweak the DataCollectorCompilerPassTest to address failures with Symfony 4.4

--- Tests/DependencyInjection/CompilerPass/DataCollectorCompilerPassTest.php
+++ Tests/DependencyInjection/CompilerPass/DataCollectorCompilerPassTest.php
@@ -3,9 +3,12 @@
 namespace Gos\Bundle\WebSocketBundle\Tests\DependencyInjection\CompilerPass;
 
 use Gos\Bundle\WebSocketBundle\DataCollector\PusherDecorator;
+use Gos\Bundle\WebSocketBundle\DataCollector\WebsocketDataCollector;
 use Gos\Bundle\WebSocketBundle\DependencyInjection\CompilerPass\DataCollectorCompilerPass;
 use Gos\Bundle\WebSocketBundle\Pusher\Wamp\WampPusher;
 use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractCompilerPassTestCase;
+use Symfony\Component\DependencyInjection\Compiler\DecoratorServicePass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
@@ -17,6 +20,7 @@ class DataCollectorCompilerPassTest extends AbstractCompilerPassTestCase
     {
         parent::setUp();
 
+        $this->container->getCompilerPassConfig()->addPass(new DecoratorServicePass(), PassConfig::TYPE_OPTIMIZE);
         $this->container->setParameter('kernel.debug', false);
     }
 
@@ -37,6 +41,7 @@ class DataCollectorCompilerPassTest extends AbstractCompilerPassTestCase
         $this->container->setParameter('kernel.debug', true);
 
         $this->registerService('debug.stopwatch', Stopwatch::class);
+        $this->registerService('gos_web_socket.data_collector.websocket', WebsocketDataCollector::class);
 
         $wampPusher = new Definition(WampPusher::class);
         $wampPusher->addTag('gos_web_socket.pusher');
