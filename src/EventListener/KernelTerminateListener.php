0889125039b5c052f8c75fa568e1de86a6b7bb66
Michael Babker <michael.babker@gmail.com>
2020-01-28 13:40:54 -0600

    Remove compat check, TerminateEvent exists in 4.4

--- src/EventListener/KernelTerminateListener.php
+++ src/EventListener/KernelTerminateListener.php
@@ -3,7 +3,6 @@
 namespace Gos\Bundle\WebSocketBundle\EventListener;
 
 use Gos\Bundle\WebSocketBundle\Pusher\PusherRegistry;
-use Symfony\Component\HttpKernel\Event\PostResponseEvent;
 use Symfony\Component\HttpKernel\Event\TerminateEvent;
 
 final class KernelTerminateListener
@@ -15,15 +14,8 @@ final class KernelTerminateListener
         $this->pusherRegistry = $pusherRegistry;
     }
 
-    /**
-     * @param PostResponseEvent|TerminateEvent $event
-     */
-    public function closeConnection(object $event): void
+    public function closeConnection(TerminateEvent $event): void
     {
-        if (!($event instanceof PostResponseEvent) && !($event instanceof TerminateEvent)) {
-            throw new \InvalidArgumentException(sprintf('The $event argument of %s() must be an instance of %s or %s, a %s was given.', __METHOD__, PostResponseEvent::class, TerminateEvent::class, \get_class($event)));
-        }
-
         foreach ($this->pusherRegistry->getPushers() as $pusher) {
             $pusher->close();
         }
