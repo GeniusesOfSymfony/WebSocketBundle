2d830db4107c786391876379d03ac133bc6c83f5
Michael Babker <michael.babker@gmail.com>
2020-05-24 20:10:28 -0500

    Remove array support from the ClientConnection class

--- UPGRADE-4.0.md
+++ UPGRADE-4.0.md
@@ -4,3 +4,4 @@
 
 - Removed the pusher and server push handler integrations, Symfony's Messenger component is the suggested replacement
 - Removed unused `gos_web_socket.client.storage.prefix` configuration node and container parameter
+- Removed `ArrayAccess` support from `Gos\Bundle\WebSocketBundle\Client\ClientConnection`
--- src/Client/ClientConnection.php
+++ src/Client/ClientConnection.php
@@ -5,7 +5,7 @@ namespace Gos\Bundle\WebSocketBundle\Client;
 use Ratchet\ConnectionInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
-final class ClientConnection implements \ArrayAccess
+final class ClientConnection
 {
     private TokenInterface $client;
     private ConnectionInterface $connection;
@@ -24,69 +24,5 @@ final class ClientConnection implements \ArrayAccess
     public function getConnection(): ConnectionInterface
     {
         return $this->connection;
-    }
-
-    /**
-     * @param int|string $offset
-     *
-     * @return bool
-     */
-    public function offsetExists($offset): bool
-    {
-        trigger_deprecation('gos/web-socket-bundle', '3.0', 'Accessing properties from %s as an array is deprecated and will be removed in 4.0, use the getters to access the properties.', self::class);
-
-        return \in_array($offset, ['client', 'connection'], true);
-    }
-
-    /**
-     * @param int|string $offset
-     *
-     * @return mixed
-     */
-    public function offsetGet($offset)
-    {
-        trigger_deprecation('gos/web-socket-bundle', '3.0', 'Accessing properties from %s as an array is deprecated and will be removed in 4.0, use the getters to access the properties.', self::class);
-
-        switch ($offset) {
-            case 'client':
-                return $this->client;
-
-            case 'connection':
-                return $this->connection;
-
-            default:
-                $trace = debug_backtrace();
-
-                trigger_error(
-                    sprintf(
-                        'Undefined property: %s in %s on line %s',
-                        $offset,
-                        $trace[0]['file'],
-                        $trace[0]['line']
-                    ),
-                    E_USER_NOTICE
-                );
-        }
-    }
-
-    /**
-     * @param int|string $offset
-     * @param mixed $value
-     *
-     * @throws \BadMethodCallException as the object is immutable
-     */
-    public function offsetSet($offset, $value): void
-    {
-        throw new \BadMethodCallException(sprintf('Properties of %s cannot be overwritten.', self::class));
-    }
-
-    /**
-     * @param int|string $offset
-     *
-     * @throws \BadMethodCallException as the object is immutable
-     */
-    public function offsetUnset($offset): void
-    {
-        throw new \BadMethodCallException(sprintf('Properties of %s cannot be unset.', self::class));
     }
 }
