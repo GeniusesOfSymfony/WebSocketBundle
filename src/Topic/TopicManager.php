ae57cbceb60bcdcf65157ef3101586e113ba9b79
Michael Babker <michael.babker@gmail.com>
2021-01-18 19:24:18 -0600

    Revert "Merge pull request #422 from webevt/3.x"
    This reverts commit e51d5a52c8a1ffd743d9711b31ece5b42d74b090, reversing
    changes made to 37e5c425cf1b2a5e7cb5f60b7f7ee29653721a35.

--- src/Topic/TopicManager.php
+++ src/Topic/TopicManager.php
@@ -90,7 +90,6 @@ class TopicManager implements WsServerInterface, WampServerInterface
 
         foreach ($this->topicLookup as $topic) {
             $this->cleanTopic($topic, $conn);
-            $this->app->onUnsubscribe($conn, $topic);
         }
     }
 
